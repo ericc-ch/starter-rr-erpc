3075b77cd3a830c40067acac8ee49e6c58578088
Erick Christian <erickchristian48@gmail.com>
2025-07-20 11:49:52 +0700

    schema ?

--- src/db/schema/books.sql.ts
+++ src/db/schema/books.sql.ts
@@ -1,7 +1,12 @@
 import * as sqlite from "drizzle-orm/sqlite-core"
 
+import { createInsertSchema, createSelectSchema } from "~/lib/drizzle-effect"
+
 export const books = sqlite.sqliteTable("books", {
   id: sqlite.integer().primaryKey({ autoIncrement: true }),
   title: sqlite.text().notNull(),
   author: sqlite.text().notNull(),
 })
+
+export const BookInsertSchema = createInsertSchema(books)
+export const BookSelectSchema = createSelectSchema(books)
